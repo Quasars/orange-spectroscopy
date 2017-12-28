346bc54ed6dd2b72198783b6c456033199a53aee
borondics <borondics.accounts@gmail.com>
2017-12-28 21:09:55 +0100

    Add spectrum shift preprocessor

--- orangecontrib/infrared/tests/test_preprocess.py
+++ orangecontrib/infrared/tests/test_preprocess.py
@@ -8,7 +8,7 @@ from orangecontrib.infrared.data import getx
 from orangecontrib.infrared.preprocess import Absorbance, Transmittance, \
     Integrate, Interpolate, Cut, SavitzkyGolayFiltering, \
     GaussianSmoothing, PCADenoising, RubberbandBaseline, \
-    Normalize, LinearBaseline
+    Normalize, LinearBaseline, CurveShift
 
 
 # Preprocessors that work per sample and should return the same
@@ -393,3 +393,22 @@ class TestPCADenoising(unittest.TestCase):
         d1 = proc(data[:0])
         newdata = Orange.data.Table(d1.domain, data)
         np.testing.assert_equal(newdata.X, np.nan)
+
+
+class TestCurveShift(unittest.TestCase):
+
+    def test_unknown_no_propagate(self):
+        data = Orange.data.Table(np.ones([4, 4]))
+        f = GaussianSmoothing()
+        for i in range(4):
+            data.X[i, i] = np.nan
+        data.X[3] = np.nan
+        fdata = f(data)
+        np.testing.assert_equal(np.sum(np.isnan(fdata.X), axis=1), [1, 1, 1, 4])
+
+    def test_simple(self):
+        data = Orange.data.Table([[1.0, 2.0, 3.0, 4.0]])
+        f = CurveShift(sd=1.1)
+        fdata = f(data)
+        np.testing.assert_almost_equal(fdata.X,
+            [[2.1, 3.1, 4.1, 5.1]])
