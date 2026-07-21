1e169185c6c4613c1eeedf12a65bc6a858e27fc6
Chris Brasnett <christopher.brasnett@diamond.ac.uk>
2026-07-21 16:25:05 +0100

    add tests for owunwrap and fix table unwrap function

--- orangecontrib/spectroscopy/widgets/owunwrap.py
+++ orangecontrib/spectroscopy/widgets/owunwrap.py
@@ -3,6 +3,7 @@ import numpy as np
 import Orange.data
 from Orange.widgets import gui, settings, widget
 from Orange.widgets.utils.concurrent import ConcurrentWidgetMixin
+from Orange.data import Table
 
 class OWUnwrap(widget.OWWidget, ConcurrentWidgetMixin):
     name = "Unwrap"
@@ -70,14 +71,22 @@ class OWUnwrap(widget.OWWidget, ConcurrentWidgetMixin):
     @staticmethod
     def unwrap_table(table, discont=DEFAULT_DISCONT, period=DEFAULT_PERIOD):
         new_table = table.copy()
-        new_table.X = OWUnwrap.unwrap(table.X, discont=discont, period=period)
-        return new_table
+        new_table_X = OWUnwrap.unwrap(new_table.X, discont=discont, period=period)
+    
+        return Table.from_numpy(
+            domain=table.domain,
+            X=new_table_X,
+            Y=table.Y.copy(),
+            metas=table.metas.copy(),
+            W=table.W.copy() 
+        )
     
 
     @staticmethod
     def unwrap(data, discont=DEFAULT_DISCONT, period=DEFAULT_PERIOD):
         new_data = data.copy()
 
+        # todo: I think this could be done as new_data = np.unwrap(data[~np.isnan(data)], axis=1, discont=discont, period=period) ? needs some testing.
         for row_i in range(data.shape[0]):
             valid = ~np.isnan(data[row_i, :])
             new_data[row_i, valid] = np.unwrap(data[row_i, valid], discont=discont, period=period)
